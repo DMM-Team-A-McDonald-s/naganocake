7bf59e1078ba625ca48492b3ada073ef1b07132f
YukiYoshizawa <eatermgsorigin4@gmail.com>
2024-12-14 07:24:41 +0000

    [add] controller/file

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,35 @@
 Rails.application.routes.draw do
+  devise_for :customers,skip: [:passwords], controllers: {
+    registrations: "public/registrations",
+    sessions: 'public/sessions'
+  }
+  devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
+    sessions: "admin/sessions"
+  }
+
+  root to: "homes#top"
+  get "home/about"=>"homes#about"
+  scope module: :public do
+    resources :items, only: [:index, :show]
+    resources :customers, only: [:show, :edit, :update]
+    get "customers/unsubscribe"=>"customers#unsubscribe"
+    patch "customers/withdraw" => "customers#withdraw"
+    resources :cart_items, only: [:index, :update, :destroy, :create]
+    delete "cart_items/destroy_all" => "cart_items#destroy_all"
+    resources :orders, only: [:new, :create, :index, :show]
+    post "orders/confirm" => "orders#confirm"
+    get "orders/complete" => "orders#complete"
+    resources :addresses, only: [:index, :edit, :create, :update, :destroy]
+  end
+
+  namespace :admin do
+    root to: "homes#top"
+    resources :items, only: [:index, :new, :create, :show, :edit, :update]
+    resources :genres, only: [:index, :create, :edit, :update]
+    resources :customers, only: [:index, :show, :edit, :update]
+    resources :orders, only: [:show, :update]
+    resources :orders_details, only: [:update]
+  end
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
