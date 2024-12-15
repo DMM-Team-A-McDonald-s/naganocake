1cf3659d31976eb1394b3147f50b83ddca3469bf
AzusaMorishita <184392623+azusaM0717@users.noreply.github.com>
2024-12-15 04:37:14 +0000

    Addviewfile

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -1,2 +1,41 @@
 class Admin::ItemsController < ApplicationController
+  
+  def index
+    @items = Item.all
+  end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to items_path
+    else
+      render:now
+    end
+  end
+
+  def show
+    @item = Item.find(params[:id])
+  end
+
+  def edit
+    @item = Item.find(params[:id])
+  end
+
+  def update
+    @item = Item.find(params[:id])
+    if @item.update
+      redirect_to items_path
+    else
+      render:edit
+    end
+  end
+
+private
+  def item_params
+    params.require(:item).permit(:name, :introduction, :price, :is_active, :image)
+  end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,11 @@
 class Item < ApplicationRecord
+  
+  validates :name, presence: true
+  validates :introduction, presence: true
+  validates :price, presence: true
+  validates :is_active, presence: true
+  validates :image, presence: true
+
+  has_one_attached :image
+  
 end
