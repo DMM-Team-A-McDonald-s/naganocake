7d3de02260b58819564d06ff6ccf2ec22af134c3
k3310 <187008159+k3310@users.noreply.github.com>
2024-12-16 06:06:06 +0000

    [Update] 会員登録画面とログイン画面のview

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -5,5 +5,5 @@ class Customer < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :last_name, :first_name, :last_name_kana, :first_name_kana, :postal_code, :address, :telephone_number, presence:{ message: 'は必須項目です。' }
-  validates :last_name_kana, first_name_kana, format: { with: /\A[\p{katakana}\p{blank}ー－]+\z/, message: 'はカタカナで入力して下さい。' }
+  validates :last_name_kana, :first_name_kana, format: { with: /\A[\p{katakana}\p{blank}ー－]+\z/, message: 'はカタカナで入力して下さい。' }
 end
