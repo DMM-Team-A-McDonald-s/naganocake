514ede10d9c32f749c0391b8b57c6fa7b5ae2076
YukiYoshizawa <eatermgsorigin4@gmail.com>
2024-12-17 08:09:50 +0000

    [add]cart_item/create

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -1,2 +1,32 @@
 class Public::CartItemsController < ApplicationController
+
+  def index
+    @cart_items = CartItem.where(customer_id: current_customer.id)
+  end
+
+  def create
+    if CartItem.find_by(item_id: params[:cart_item][:item_id], customer_id: current_customer.id).nil?
+      # カート内に同じアイテムがある場合は数量のみを追加するための条件分岐
+      @cart_item = CartItem.new(cart_item_params)
+      if @cart_item.save
+        redirect_to cart_items_path
+      else
+        redirect_to items_path
+        # ここは仮
+      end
+    else
+      @cart_item = CartItem.find_by(item_id: params[:cart_item][:item_id], customer_id: current_customer.id)
+      @cart_item.amount = @cart_item.amount + params[:cart_item][:amount].to_i
+      @cart_item.save
+      redirect_to cart_items_path
+      # 同じアイテムがあった場合amountのみを追加して保存する
+    end
+
+  end
+
+  private 
+  
+  def cart_item_params
+    params.require(:cart_item).permit(:item_id, :customer_id, :amount)
+  end
 end
