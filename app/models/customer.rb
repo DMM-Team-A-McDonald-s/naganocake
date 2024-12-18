487cac75b3f26ab953101c3a4c189deb09e7d427
k3310 <187008159+k3310@users.noreply.github.com>
2024-12-18 01:01:12 +0000

    Fix model_customer

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -7,11 +7,5 @@ class Customer < ApplicationRecord
   validates :last_name, :first_name, :last_name_kana, :first_name_kana, :postal_code, :address, :telephone_number, presence:{ message: 'は必須項目です。' }
   validates :last_name_kana, :first_name_kana, format: { with: /\A[\p{katakana}\p{blank}ー－]+\z/, message: 'はカタカナで入力して下さい。' }
 
-  def full_name
-    self.last_name + " " + self.first_name
-  end
-
-  def full_name_kana
-    self.last_name_kana + " " + self.first_name_kana
-  end
+  
 end
