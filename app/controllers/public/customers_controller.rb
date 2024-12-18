c082518807294f49a2415ec64d46c8861342b666
k3310 <187008159+k3310@users.noreply.github.com>
2024-12-18 07:41:43 +0000

    Add unsubscribe, withdraw

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -19,6 +19,16 @@ class Public::CustomersController < ApplicationController
     end
   end
 
+  def unsubscribe
+  end
+
+  def withdraw
+    @customer = current_customer
+    @customer.update(is_active: false)
+    reset_session
+    redirect_to root_path
+  end
+
   private
 
   def customer_params
