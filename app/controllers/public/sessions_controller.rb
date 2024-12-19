6406eca7f6f609b451bdb082e4cf7ec952d1b379
k3310 <187008159+k3310@users.noreply.github.com>
2024-12-19 01:04:42 +0000

    Add customer_state

--- app/controllers/public/sessions_controller.rb
+++ app/controllers/public/sessions_controller.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 class Public::SessionsController < Devise::SessionsController
+  before_action :customer_state, only: [:create]
   # before_action :configure_sign_in_params, only: [:create]
 
   # GET /resource/sign_in
@@ -31,6 +32,16 @@ class Public::SessionsController < Devise::SessionsController
 
   def after_sign_out_path_for(resource)
     root_path
+  end
+
+  private
+
+  def customer_state
+    customer = Customer.find_by(:email params[:custome][:email])
+    return if customer.nil?
+    return unless customer.valid_password?(params[:customer][:password])
+    if cusotmer.is_active == false
+      redirect_to new_customer_registration_path
   end
 
 end
