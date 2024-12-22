77ebe900272d81ecc0c09e0aaff5b5e0ee1431d1
k3310 <187008159+k3310@users.noreply.github.com>
2024-12-22 08:03:52 +0000

    Add validation to customer

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -10,5 +10,7 @@ class Customer < ApplicationRecord
 
   validates :last_name, :first_name, :last_name_kana, :first_name_kana, :postal_code, :address, :telephone_number, presence:{ message: 'は必須項目です。' }
   validates :last_name_kana, :first_name_kana, format: { with: /\A[\p{katakana}\p{blank}ー－]+\z/, message: 'はカタカナで入力して下さい。' }
-  validates :telephone_number, numericality: { only_integer: true}
+  validates :postal_code, numericality: { only_integer: true}, length: { is: 7 }
+  validates :address, length: { maximum: 100 }
+  validates :telephone_number, numericality: { only_integer: true}, length: { in: 10..11 }
 end
