24e09e0c0bc6a692b636b2be676e1a5a4c17d1df
YukiYoshizawa <eatermgsorigin4@gmail.com>
2024-12-24 02:08:05 +0000

    [fix] items/controller items/showw

--- app/controllers/public/items_controller.rb
+++ app/controllers/public/items_controller.rb
@@ -2,14 +2,14 @@ class Public::ItemsController < ApplicationController
   before_action :authenticate_customer!, except: [:index, :show]
 
 def index
-  @total_items = Item.count
+  @total_items = Item.where(is_active: true).count
   @genres = Genre.all
 
   if params[:genre_id].present?
     @genre = Genre.find(params[:genre_id])
-    @items = Item.where(genre_id: params[:genre_id]).page(params[:page]).per(8)
+    @items = Item.where(genre_id: params[:genre_id], is_active: true).page(params[:page]).per(8)
   else
-    @items = Item.page(params[:page]).per(8)
+    @items = Item.where(is_active: true).page(params[:page]).per(8)
   end
 end
 
